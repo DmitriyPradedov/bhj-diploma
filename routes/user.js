42c63277e585c8a67a670b1dba7496d52b6e788b
DmitriyPradedov <pradedov72@mail.ru>
2022-09-26 14:51:29 +0700

    испралено

--- routes/user.js
+++ routes/user.js
@@ -15,34 +15,36 @@ router.post("/register",upload.none(), function(request, response) {
     //получение параметров из тела запроса
     const { name, email, password } = request.body;
     //формирование ошибки (не обязательна, но желательна т.к. валидация есть на UI)
-    error = "";
+    error = {};
     if(name === "")
-        error += 'Поле Имя обязательно для заполнения. ';
+        error.name = [ 'Поле Имя обязательно для заполнения.' ];
 
     if(email === "")
-        error += 'Поле E-Mail адрес для заполнения. ' ;
+        error.email = [ 'Поле E-Mail адрес для заполнения.' ];
 
     if(password === "")
-        error += 'Поле Пароль обязательно для заполнения.';
+        error.password = [ 'Поле Пароль обязательно для заполнения.' ];
     
     //если ошибка сформирована...
-    if(error !== "")
-        response.json({success: false, error});//отправляем ошибку
+    if(JSON.stringify(error) !== "{}")
+        response.json({ success: false, error});//отправляем ошибку
 
     //нахождение такого же пользователя по email
     let user = db.get("users").find({email}).value();
     if(!user){//если существующий пользователь не найден...
         //создаётся пользователя
-        user = { name, email, password, id:uniqid() };
+        user = { name, email, password, id:uniqid(), isAuthorized:true };
         //записывается в БД
         db.get("users").push(user).write();
-        request.session.id = user.id;
+        //удаляются лишние поля, которые не нужны для Front-end'a
+        delete user.password;
+        delete user.isAuthorized;
         //отправляется созданный пользователь
-        response.json({success: true, user});
+        response.json({ success: true, user});
     }
     else{//если существующий пользователь найден...
         //Отправляется ошибка о том, что пользователь такой уже существует
-        response.json({success: false, error: `E-Mail адрес ${email} уже существует.`});
+        response.json({ success: false, error: { email:`E-Mail адрес ${email} уже существует.`}});
     }
 })
 
@@ -55,41 +57,43 @@ router.post("/login",upload.none(), function(request, response) {
     let user = db.get("users").find({email, password});
     let foundedUser = user.value();//получение из БД значения пользователя
     if(!!foundedUser){//если пользователь существует...
-        request.session.id = foundedUser.id;
+        user.assign({isAuthorized: true}).write();//записывается в БД флаг его авторизованности
+        //удаляются лишние поля, которые не нужны для Front-end'a
+        delete foundedUser.password;
+        delete foundedUser.isAuthorized;
         //отправляется авторизованный пользователь
-        response.json({success: true, user: foundedUser});
+        response.json({ success: true, user: foundedUser});
     }
     else//если пользователь не существует, то отправляется ответ с ошибкой о ненахождении пользователя
-        response.json({success: false, error:`Пользователь c email ${email} и паролем ${password} не найден`});
+        response.json({ success: false, error:`Пользователь c email ${email} и паролем ${password} не найден`});
 })
 
 //запрос разлогина пользователя
 router.post("/logout", function(request, response) {
-    if (request.session.id) {
-        delete request.session.id;
-        response.json({success: true});
-    } else {
-        //отправляется ответ успешности
-        response.json({success: false, error: 'Пользователь не авторизован'});
-    }
+    const db = low(new FileSync('db.json'));// получение БД
+    //находится первый авторизованный пользователь, ему присваивается флаг авторизации и записывается в БД
+    db.get("users").find({isAuthorized: true}).assign({isAuthorized: false}).write();
+    //отправляется ответ успешности
+    response.json({ success: true});
 })
 
 //запрос получения текущего пользователя
 router.get("/current", function(request, response) {
     const db = low(new FileSync('db.json'));// получение БД
-    let { id } = request.session; // получение id пользователя из запроса
+    let { id } = request.query; // получение id пользователя из запроса
     //получение из БД пользователя с переданным id
     let user = db.get("users").find({id});
     let userValue = user.value();//получение значения из БД
-    if(userValue){//если пользователь найден и он авторизован...
-        //удаляется пароль, который не нужен для Front-end'a
+    if(userValue && userValue.isAuthorized){//если пользователь найден и он авторизован...
+        //удаляются лишние поля, которые не нужны для Front-end'a
         delete userValue.password;
+        delete userValue.isAuthorized;
         //отправка ответа пользователем
-        response.json({success: true, user: userValue});
+        response.json({ success: true, user: userValue });
     }
     else{
         //отправка ответа с отсутствием пользователя
-        response.json({success: false, user: null, error: 'Пользователь не авторизован'});
+        response.json({ success: false, user: null, error: 'Необходимо передать id, name и email пользователя' });
     }
 })
 
